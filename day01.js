a64e93e556b985678bf6b1a5ba503092c0e12dea
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2019-12-05 00:25:24 +0100

    day 1 puzzle 2 solved

--- day01.js
+++ day01.js
@@ -1,7 +1,7 @@
 const readInput = require("./helpers");
 const fs = require("fs").promises;
 
-async function calcFuelComsumption() {
+async function fuelConsumption() {
   try {
     const massList = await readInput.readInput(1);
 
@@ -10,14 +10,23 @@ async function calcFuelComsumption() {
     let sum = 0;
 
     massArr.forEach(mass => {
-      const consumption = Math.floor(mass / 3) - 2;
+      let consumption = calculate(mass);
       sum += consumption;
+      // remove this while loop for the solution to puzzle 1
+      while (calculate(consumption) > 0) {
+        consumption = calculate(consumption);
+        sum += consumption;
+      }
     });
     console.log(sum);
-    await fs.writeFile(`${__dirname}/output/d01p01.txt`, sum);
+    await fs.writeFile(`${__dirname}/output/d01p02.txt`, sum);
   } catch (err) {
     console.error(err);
   }
 }
 
-calcFuelComsumption();
+function calculate(mass) {
+  return Math.floor(mass / 3) - 2;
+}
+
+fuelConsumption();
