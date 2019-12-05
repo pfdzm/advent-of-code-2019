a90652f526714c3b42fcf0a99e6f401ae866f3ad
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2019-12-06 00:39:42 +0100

    day 2 puzzle 2 solved

--- day02.js
+++ day02.js
@@ -4,27 +4,46 @@ async function intcode() {
   try {
     let input = await helpers.readInput(2);
     input = input.split(",").map(elem => parseInt(elem));
-    // array is processed in chunks of 4
-    for (let index = 0; index < input.length; index += 4) {
-      let opcode = input[index];
-      let operand1 = input[index + 1];
-      let operand2 = input[index + 2];
-      let result = input[index + 3];
-      if (opcode == 1) {
-        // add
-        input[result] = input[operand1] + input[operand2];
-      } else if (opcode == 2) {
-        // multiply
-        input[result] = input[operand1] * input[operand2];
-      } else {
-        // abort
-        break;
-      }
+    let output = [];
+    let noun, verb;
+    while (output[0] !== 19690720) {
+      noun = Math.floor(Math.random() * 100);
+      verb = Math.floor(Math.random() * 100);
+      input[1] = noun; //45
+      input[2] = verb; //59
+      // array is processed in chunks of 4
+
+      output = calculate(input);
+      console.log(output[0]);
     }
-    console.log(input.join(","));
+
+    console.log(100 * noun + verb);
+
+    // console.log(input.join(","));
   } catch (err) {
     console.error(err);
   }
+}
+
+function calculate(input) {
+  let output = input.slice();
+  for (let index = 0; index < output.length; index += 4) {
+    let opcode = output[index];
+    let operand1 = output[index + 1];
+    let operand2 = output[index + 2];
+    let result = output[index + 3];
+    if (opcode == 1) {
+      // add
+      output[result] = output[operand1] + output[operand2];
+    } else if (opcode == 2) {
+      // multiply
+      output[result] = output[operand1] * output[operand2];
+    } else {
+      // abort
+      break;
+    }
+  }
+  return output;
 }
 
 intcode();
